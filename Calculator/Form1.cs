1f692279dd93a842bd48866b2b6f02ef568c9cad
brooklynnsilva <brooklynnsilva@gmail.com>
2023-09-28 19:41:15 -0600

    Added all buttons and resized

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Calculator
 {
-    public partial class Form1 : Form
+    public partial class Calc_Form : Form
     {
-        public Form1()
+        public Calc_Form()
         {
             InitializeComponent();
         }
